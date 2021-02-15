4350ac5289958c0ede30988c254418517e7362e9
juhwano <calculus9006@gmail.com>
2021-02-15 10:18:10 +0900

    add Ex08_Collection

---
+++ Ex08_Collection/src/Ex01_Vector.java
@@ -0,0 +1,22 @@
+/*
+
+Collection FrameWork
+"다수의 데이터"를 다루는 "표준화된 인터페이스"를 구현하고 있는 "클래스 집합"
+
+Collection 인터페이스  ->  List 인터페이스  -> ArrayList(구현 : 수많은 약속을)
+                       ->  Set  인터페이스  -> HashSet, TreeSet(구현)
+ 
+ 
+Map 인터페이스 ( key, value)  ->HashMap(구현)
+
+1. List(줄을 서시오: 번호표) : 순서를 보장, 중복 허용 >> 내부적으로 데이터(자료, 객체) >> 
+
+*/
+public class Ex01_Vector {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
