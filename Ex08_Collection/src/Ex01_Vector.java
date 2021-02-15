7d74b39503f3436f99edeac7556907c139cc13f7
juhwano <calculus9006@gmail.com>
2021-02-15 10:33:43 +0900

    delete file

--- Ex08_Collection/src/Ex01_Vector.java
+++ Ex08_Collection/src/Ex01_Vector.java
@@ -2,14 +2,23 @@
 
 Collection FrameWork
 "다수의 데이터"를 다루는 "표준화된 인터페이스"를 구현하고 있는 "클래스 집합"
+자료구조  :자료를 저장할 수 있는 구조
+다양한 자료들을 다양한 방식으로 관리
+
+
 
 Collection 인터페이스  ->  List 인터페이스  -> ArrayList(구현 : 수많은 약속을)
                        ->  Set  인터페이스  -> HashSet, TreeSet(구현)
- 
- 
-Map 인터페이스 ( key, value)  ->HashMap(구현)
 
-1. List(줄을 서시오: 번호표) : 순서를 보장, 중복 허용 >> 내부적으로 데이터(자료, 객체) >> 
+ 
+Map 인터페이스 (key, value)  -> HashMap(구현)
+
+1. List(줄을 서시오: 번호표) : 순서 보장, 중복 허용 >> 내부적으로 데이터(자료, 객체)를 "배열"로 관리하겠다
+예) [홍길동][홍길동][홍길동]
+
+구현하고 있는 클래스
+1.1 Vector(구버전)
+1.2 ArrayList(신버전)
 
 */
 public class Ex01_Vector {
